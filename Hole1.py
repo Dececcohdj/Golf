ed9ac6d92675738408f048f83a52eae04251dc5d
Dececcohdj <66085798+Dececcohdj@users.noreply.github.com>
2020-11-22 18:50:37 -0800

    Add files via upload
    -added tallgrass frames
    -added hole
    -added code for tallgrass animation

---
+++ Hole1.py
@@ -0,0 +1,56 @@
+from os import path
+import sys
+
+from kivy.app import App
+from kivy.uix.image import Image
+from kivy.uix.label import Label
+from kivy.properties import ObjectProperty, ListProperty
+from kivy.lang import Builder
+
+
+kv = '''
+<LabelOnBackground>:
+    canvas.before:
+        Color:
+            rgb: self.background
+        Rectangle:
+            pos: self.pos
+            size: self.size
+
+FloatLayout:
+    canvas.before:
+        Color:
+            rgb: 1, 1, 1
+        Rectangle:
+            pos: self.pos
+            size: self.size
+            texture: app.texture
+
+    LabelOnBackground:
+        text: '{} (try to resize the window)'.format(root.size)
+        color: (0.4, 1, 1, 1)
+        background: (.3, .3, .3)
+        pos_hint: {'center_x': .5, 'center_y': .5 }
+        size_hint: None, None
+        height: 30
+        width: 250
+
+'''
+
+
+class LabelOnBackground(Label):
+    background = ListProperty((0.2, 0.2, 0.2))
+
+
+class RepeatTexture(App):
+
+    texture = ObjectProperty()
+
+    def build(self):
+        self.texture = Image(source='sprites/small.gif').texture
+        self.texture.wrap ='repeat'
+        self.texture.uvsize = (8, 8)
+        return Builder.load_string(kv)
+
+
+RepeatTexture().run()
